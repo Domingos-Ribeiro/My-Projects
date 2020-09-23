a99a93885a14b6b122ade60bed4d897a41b57d2f
Domingos Ribeiro <domingos.ribeiro.net@gmail.com>
2020-09-23 23:46:25 +0100

    Menu and Background

--- Form1.h
+++ Form1.h
@@ -34,6 +34,14 @@ namespace ProjetoCLR {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^ fileToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ editToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ viewToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ projectsToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ projectsToolStripMenuItem1;
+	private: System::Windows::Forms::ToolStripMenuItem^ helpToolStripMenuItem;
 
 	private:
 		/// <summary>
@@ -48,16 +56,82 @@ namespace ProjetoCLR {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->fileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->editToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->viewToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->projectsToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->projectsToolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->helpToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(6)
+				{
+				this->fileToolStripMenuItem,
+					this->editToolStripMenuItem, this->viewToolStripMenuItem, this->projectsToolStripMenuItem, this->projectsToolStripMenuItem1,
+					this->helpToolStripMenuItem
+				});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(784, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this->fileToolStripMenuItem->Name = L"fileToolStripMenuItem";
+			this->fileToolStripMenuItem->Size = System::Drawing::Size(37, 20);
+			this->fileToolStripMenuItem->Text = L"File";
+			// 
+			// editToolStripMenuItem
+			// 
+			this->editToolStripMenuItem->Name = L"editToolStripMenuItem";
+			this->editToolStripMenuItem->Size = System::Drawing::Size(39, 20);
+			this->editToolStripMenuItem->Text = L"Edit";
+			// 
+			// viewToolStripMenuItem
+			// 
+			this->viewToolStripMenuItem->Name = L"viewToolStripMenuItem";
+			this->viewToolStripMenuItem->Size = System::Drawing::Size(44, 20);
+			this->viewToolStripMenuItem->Text = L"View";
+			// 
+			// projectsToolStripMenuItem
+			// 
+			this->projectsToolStripMenuItem->Name = L"projectsToolStripMenuItem";
+			this->projectsToolStripMenuItem->Size = System::Drawing::Size(61, 20);
+			this->projectsToolStripMenuItem->Text = L"Courses";
+			// 
+			// projectsToolStripMenuItem1
+			// 
+			this->projectsToolStripMenuItem1->Name = L"projectsToolStripMenuItem1";
+			this->projectsToolStripMenuItem1->Size = System::Drawing::Size(61, 20);
+			this->projectsToolStripMenuItem1->Text = L"Projects";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this->helpToolStripMenuItem->Name = L"helpToolStripMenuItem";
+			this->helpToolStripMenuItem->Size = System::Drawing::Size(44, 20);
+			this->helpToolStripMenuItem->Text = L"Help";
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(478, 517);
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(784, 612);
+			this->Controls->Add(this->menuStrip1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::Fixed3D;
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"Form1";
 			this->Text = L"Computer Programming Course - Initiation (216 Hours)";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 			}
 #pragma endregion
